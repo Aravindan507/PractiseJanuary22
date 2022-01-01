74acde52b4ada9e9b3946ab536c5ee44a1046859
Aravindan <aravindansrinivas96@gmail.com>
2022-01-01 18:57:02 +0530

    im commiting

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeAppsAndroid()
+	{
+		System.out.println("nativeAppsAndroid");
+	}
+
+	@Test
+	public void iosApp()
+	{
+		System.out.println("iosApp");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserAutomationSelenium()
+	{
+		System.out.println("browserAutomationSelenium");
+	}
+
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+}
